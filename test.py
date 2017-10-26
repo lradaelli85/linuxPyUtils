6720f8adbeea3b815d41cd916c4291ef764d20b9
Luca Radaelli <lradaelli85@users.noreply.github.com>
2017-10-26 15:29:20 +0200

    added function that cheks deb dependencies

--- test.py
+++ test.py
@@ -1,49 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-.
 
-#import subprocess
-#import shlex
+
 from utils.Apyt import apyt
 from utils.linuxOsUtils import LinuxOsUtils
 
 if __name__ == "__main__":
-    #cmd1 = shlex.split('/usr/bin/apt-cache search -n -q vima')
-    #cmd2 = shlex.split("awk '($1==\"vima\")'")
-    #cmd3 = shlex.split('wc -l')
-    #p0 = subprocess.Popen(cmd1,stdout=subprocess.PIPE)
-    #p1 = subprocess.Popen(cmd2,stdout=subprocess.PIPE ,stdin=p0.stdout,stderr=subprocess.PIPE)
-    #p2 = subprocess.check_output(cmd3,stdin=p1.stdout,stderr=subprocess.PIPE)
-    #
-    #p1.communicate()
-    #p0.wait()
-    #print p2
-    #print p0.wait()
-
-    pack='vimsdf'
     a = apyt()
-    b = LinuxOsUtils()
-    # print 'whoami',b.am_i_root()
-    # print 'linux_arch',b.get_linux_arch()
-    # print 'linux_distro',b.get_linux_distro()
-    # print 'list dir',b.list_dir_objects('/home/luke')
-
-    # uno = a.search_in_repo(pack)
-    # if uno:
-    #     print pack,' present in repo'
-    # else:
-    #     print pack,' is not present in repo'
-    # due = a.check_if_deb_is_installed(pack)
-    # if due == True:
-    #     print pack,' is installed'
-    # elif due == 'Half':
-    #     print pack,' has been removed but config files exist'
-    # else:
-    #     print pack,' is not installed'
-    tre = b.search_in_file('luke', '/etc/passwd')
-    print tre
-    quattro = b.search_pattern_in_folder('lukes','/etcs/')
-    if type(quattro) is bool:
-        print quattro
-    else:
-        print type(quattro)
-        print quattro[1]
+    a.check_deb_dependencies('/home/luke/Downloads/google-chrome-stable_current_amd64.deb')
+    #print a.check_if_deb_is_installed('google-chrome-stable',check_version=True)
--- utils/Apyt.py
+++ utils/Apyt.py
@@ -20,18 +20,25 @@ class apyt():
         else:
             return True
 
-    def check_if_deb_is_installed(self,package):
+    def check_if_deb_is_installed(self,package,check_version=False):
         '''Search if a package is installed (Debian based distro)
+           returns a tuple with True,Half,False and packet version (if checked)
            return True if it is installed,False if it is not installed and Half
            if it has been removed but configuration file exist
         '''
         is_installed = Command("/usr/bin/dpkg-query -W -f='${Status}' %s" %package).run(OnlyOutPut=True)
-        if "install ok installed" in is_installed:
-            return True
-        elif "deinstall ok config-files" in is_installed:
-            return 'Half'
+        if check_version:
+            version = Command("/usr/bin/dpkg-query -W -f='${Version}' %s" %package).run(OnlyOutPut=True)
         else:
-            return False
+            version = False
+        if "install ok installed" in is_installed:
+            is_installed = True
+        elif "deinstall ok config-files" in is_installed:
+            is_installed = 'Half'
+        else:
+            is_installed = False
+        return is_installed,version
+
 
     def inst_from_repo(self,package):
         '''Install a package from Linux repo (Debian based distro)'''
@@ -58,3 +65,14 @@ class apyt():
                             inst_a_deb(package)
                 else:
                     print 'empty folder'
+
+    def check_deb_dependencies(self,package):
+        '''check dependencies of a .deb package'''
+        dependencies = Command('dpkg -f {} Depends'.format(package)).run(OnlyOutPut=True)
+        for dep in dependencies.split(','):
+            package = dep.split()[0]
+            if len(dep.split()) > 1:
+                version = dep.split()[1].strip('(')+dep.split()[2].strip(')')
+                print '{} version: {}'.format(package,version)
+            else:
+                print package
